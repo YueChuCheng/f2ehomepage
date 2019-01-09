f277d1d36e85f9385dbf4a6997f348d977fd2b72
YueChuCheng <39110560+YueChuCheng@users.noreply.github.com>
2019-01-09 11:47:12 +0800

    Add files via upload

---
+++ js/script2.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+   
+    for (var i = 1; i <= 12; i++) {
+        $("div.img"+i).css("background-image", 'url(../image/clothes/clothes'+i+'.jpg)');
+        $("div.img"+i).css("background-size", "cover");
+    };
+
+    $(".row_select_black").click(function (event) { 
+
+        for (var i = 1; i <= 12; i++) {
+        $("div.img"+i).css("background-image", 'url(../image/clothes/clothes'+i+'.jpg)');
+        $("div.img"+i).css("background-size", "cover");
+    };
+        
+    });
+    $(".row_select_blue").click(function (event) { 
+
+        for (var i = 1; i <= 12; i++) {
+            $("div.img"+i).css("background-image", 'url(../image/suitting/suitting'+i+'.jpg)');
+            $("div.img"+i).css("background-size", "cover");
+        };
+        
+    });
+    $(".row_select_yellow").click(function (event) { 
+
+        for (var i = 1; i <= 12; i++) {
+            $("div.img"+i).css("background-image", 'url(../image/shoes/shoes'+i+'.jpg)');
+            $("div.img"+i).css("background-size", "cover");
+        };
+        
+    });
+    $(".row_select_gray").click(function (event) { 
+
+        for (var i = 1; i <= 12; i++) {
+            $("div.img"+i).css("background-image", 'url(../image/jeans/jeans'+i+'.jpg)');
+            $("div.img"+i).css("background-size", "cover");
+        };
+        
+    });
+
+
+});
